2435cf585f23dc0ed46958dd0da97910e6d62607
rewatiramangautam <rewatiramangautam@nbnco.com.au>
2018-07-27 11:08:34 +1000

    added aws-role to create prod infra

---
+++ aws-prod/roles/aws-prod/files/aws_collect_dependencies.sh
@@ -0,0 +1,5 @@
+mkdir ~/bin
+wget http://www.opentlc.com/download/ansible_bootcamp/scripts/common.sh
+wget http://www.opentlc.com/download/ansible_bootcamp/scripts/jq-linux64 -O ~/bin/jq
+wget http://www.opentlc.com/download/ansible_bootcamp/scripts/order_svc.sh
+chmod +x order_svc.sh ~/bin/jq
